7fd2c1e4cf620cb6a0805d23b1d79f83c6b276f1
tgaff <tgaff@alumni.nd.edu>
2016-02-05 16:53:38 -0800

    it looks much better
    but code is scary for teaching

--- client/js/app.js
+++ client/js/app.js
@@ -8,30 +8,32 @@ function hangmanController() {
   console.log('hangmanController online');
   this.controllerWorks = 'ok';
   this.secretWord = 'elephant';
-  this.completedWord = '________';
   this.guesses = [];
+  this.completedWord = updateCompletedWord();
   this.triesRemaining = 7;
   this.guess = '';
 
   this.checkGuess = function() {
-    var letter = this.guess;
-    console.log('you guessed', letter);
-    if (isLetterInWord(letter, this.secretWord)) {
-      console.log('found ' + letter + ' in the word: ', this.secretWord);
+    var newLetter = this.guess;
+    console.log('you guessed', newLetter);
+    if (isLetterInWord(newLetter, this.secretWord)) {
+      console.log('found ' + newLetter + ' in the word: ', this.secretWord);
     } else {
       this.triesRemaining--;
     }
     // note I would have liked this to use $watch but
     // http://stackoverflow.com/questions/24078535/angularjs-controller-as-syntax-and-watch#24078893
-    updateCompletedWord(this.guess);
-    checkGameStatus();
+    vm.guesses.push(newLetter);
+    // update the completedWord
+    updateCompletedWord();
+    checkGameWinStatus();
     this.guess='';
   };
-  function checkGameStatus() {
+  function checkGameWinStatus() {
     if(vm.triesRemaining === 0) {
       alert('YOU LOSE!!!!!!!!!!!!!!!!!!!!');
       return false;
-    } else if( !isLetterInWord(/\_/, vm.completedWord) ) {
+    } else if( !isLetterInWord('_', vm.completedWord) ) {
       alert('YOU WIN!!!!!!!!!!!!!!!!!!!!!!!!');
       return false;
     } else {
@@ -40,30 +42,24 @@ function hangmanController() {
     }
   }
 
+  // checks if the letter you passed is in the word
+  // returns true or false
   function isLetterInWord(letter, word) {
-    return ((word.search(letter) > -1) ? (true) : (false));
+    return ((word.split('').indexOf(letter) > -1) ? (true) : (false));
   }
 
-  function updateCompletedWord(newLetter) {
-    console.log(vm.guesses);
-    vm.guesses.push(newLetter);
-    console.log('guesses', vm.guesses, vm);
-
+  function updateCompletedWord() {
     var newSecretWord = '';
     for (var index in vm.secretWord) {
       var currentLetter = vm.secretWord[index];
-      console.log('cL, vmg', currentLetter, vm.guesses);
       if(vm.guesses.indexOf(currentLetter) > -1) {
-        console.log(newSecretWord);
         newSecretWord += currentLetter;
       } else {
         newSecretWord += '_';
       }
     }
     vm.completedWord = newSecretWord;
-    console.log(vm.completedWord);
-    console.log(vm);
-
+    return newSecretWord;
   }
 
 }
