1f670e30a988fe93129c8dd375e02f8698a1fbcc
tgaff <tgaff@alumni.nd.edu>
2016-02-06 18:04:54 -0800

    reveal entire game object so we don't need to explicitly update controller properties

--- client/js/app.js
+++ client/js/app.js
@@ -3,6 +3,32 @@ console.log('cookies');
 var app = angular.module("hangmanApp", [])
   .controller('hangmanController', hangmanController);
 
+
+  function hangmanController() {
+    var vm = this;
+    console.log('hangmanController online');
+    this.controllerWorks = 'ok';
+    this.game = new HangmanGame('elephant');
+    this.guess = '';
+    this.checkGuess = checkGuess;
+
+    // define functions separately to keep code easy to read
+    // this function sends the new guess to the hangmanGame which updates accordingly
+    // it then clears the input field
+    function checkGuess() {
+      var guess = this.guess;
+      this.game.guess(guess);
+      this.guess = '';
+    }
+  }
+
+
+// Alternate solution using multiple primitives/scalars for the view.  Note how in this case,
+// we explicitly update properties on the controller.  These are copies, from the
+// game and so won't update on their own, (outside of angular bindings).
+// Note also that when using this syntax index.html should be updated to reference
+//   these properties directly: e.g. use hangman.guesses rather than hangman.game.guesses
+/*
 function hangmanController() {
   var vm = this;
   console.log('hangmanController online');
@@ -18,18 +44,16 @@ function hangmanController() {
   // this function sends the new guess to the hangmanGame which updates accordingly
   // it then clears the input field
   function checkGuess() {
-    // console.log('this', this);
-    // console.log('vm', vm);
     var guess = this.guess;
     game.guess(guess);
     updateState();
   }
-  //updates the scalar values with the values from the game
+  //copies primitives
   function updateState() {
     vm.completedWord = game.completedWord;
     vm.triesRemaining = game.triesRemaining;
     vm.guess = '';
   }
 
-
 }
+*/
